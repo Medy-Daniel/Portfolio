a1f34a6bd30b51ae145c154b5ed2bf538064e380
Medy-Daniel <medy.vilgenis@gmail.com>
2024-07-11 18:38:03 +0200

    wip-switch-background

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,18 +1,30 @@
+"use client"
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Github } from "../icons/Github";
 import { Section } from "./Section"; 
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { Linkedin } from "../icons/Linkedin";
+import { useState } from "react";
 
 
  export const Header = () => { 
+
+    const [blueMode, setBlueMode] = useState(false);
+
+  const toggleBlueMode = () => {
+    setBlueMode(!setBlueMode);
+    document.documentElement.classList.toggle('white'); // Ajout/suppression de la classe 'dark'
+  };
+    
+
     return (
     <header className=" top-0 py-4">
         <Section className= "flex items-baseline overflow-x-hidden">
             <h1 className="text-lg font-bold text primary-foreground">
                Medy Daniel 
             </h1>
+            <button className="ml-20 bg-white rounded-sm text-black" onClick={toggleBlueMode}>Switch</button>
             <div className="flex-1" />
             <ul>
                 <Link
